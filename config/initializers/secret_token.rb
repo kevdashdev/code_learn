0bef812c9d454f08bbef3ed7f29b552307e4a99d
kevdashdev <kevdashdev@gmail.com>
2013-06-08 08:48:54 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeLearn::Application.config.secret_token = '50984642a9301748a07292d5a7cb4785b8774ecbe8345e598e4436a7f12fd03b2230cea9397f115e08910e1959376f7e49b846d3d7c14689f73fd0cfb646ce63'
